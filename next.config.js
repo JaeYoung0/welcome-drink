1c018f2d1b3b7b6f3d4ea25fb13dc3d55f84c27c
JY <jy@JYui-Macmini.local>
2021-08-06 03:27:48 +0900

    fix: next.config.js - withPlugins

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 const withPWA = require("next-pwa");
+const withPlugins = require("next-compose-plugins");
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
-  reactStrictMode: true,
-});
+module.exports = withPlugins([
+  [
+    withPWA({
+      pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+        sw: "/sw.js",
+      },
+      reactStrictMode: true,
+    }),
+  ],
+]);

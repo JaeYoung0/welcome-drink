1c9a663053724066813105a58d16187bf57ab109
JY <jy@JYui-Macmini.local>
2021-08-06 03:36:15 +0900

    fix: build error

--- next.config.js
+++ next.config.js
@@ -1,15 +1,22 @@
 const withPWA = require("next-pwa");
 const withPlugins = require("next-compose-plugins");
 
-module.exports = withPlugins([
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = withPlugins(
   [
-    withPWA({
-      pwa: {
-        dest: "public",
-        register: true,
-        skipWaiting: true,
-        sw: "/sw.js",
-      },
-    }),
+    // [
+    //   withPWA({
+    //     pwa: {
+    //       dest: "public",
+    //       register: true,
+    //       skipWaiting: true,
+    //       sw: "/sw.js",
+    //     },
+    //   }),
+    // ],
   ],
-]);
+  nextConfig
+);
